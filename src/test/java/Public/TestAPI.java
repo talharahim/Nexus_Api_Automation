7e92575b2c93d2173324dad95ec2d11c4363dcef
Talha <talharahim@gmail.com>
2022-07-07 01:09:36 -0700

    .

--- src/test/java/Public/TestAPI.java
+++ src/test/java/Public/TestAPI.java
@@ -18,26 +18,21 @@ public class TestAPI {
 
 	public static JsonPath jsonPathEvaluator;
 
-	@Test(priority = 7, groups = "Cashering")
-	public void TC007_getAutoApply() throws ClassNotFoundException, SQLException, InterruptedException {
-		// CommonMethods.CompanyDBRestore();
-		String uri = "/cashiering/autoApply";
+	@Test(priority = 3, groups = "CustomerController")
+	public void getlocationsByCustomerv_2_4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+
+		String uri = "/customers/AUTO1001/locationsByCustomer";
 		String ver = "2.4";
-		Map<String, String> responseMap = new HashMap<String, String>();
-		responseMap.put("CustomerId", "CUSTOMER010");
-		responseMap.put("LocationId", "LOCATION009");
-		responseMap.put("ReceiptNumber", "0123555555");
-		responseMap.put("ApplyAmount", "50.00");
-		responseMap.put("PaymentOrigin", "TEST");
-		jsonPathEvaluator = CommonMethods.getMethod(uri, ver, responseMap);
-		System.out.println(jsonPathEvaluator.get().toString());
-		// String Result = jsonPathEvaluator.get("CashieringTransaction.CustomerId");
-		String Result = jsonPathEvaluator.getJsonObject("CashieringTransaction.CustomerId[0]");
- 
-		if (!Result.contentEquals("CUSTOMER010"))
-			Assert.fail(Result);
-			Result = jsonPathEvaluator.get("CashieringTransaction.Document[0].Number[0]");
-		if (!Result.contentEquals("BILL00000000374"))
-			Assert.fail(Result);
+		String jpath = "./\\TestData\\locationsByCustomerv2_4.json";
+				
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("LocationId", "LOCATION008");
+		params.put("NumPerPage", "50");
+		params.put("OrderBy", "status, locationId");
+		params.put("PageNum", "1");
+				
+		String result = CommonMethods.getMethod(uri, ver, params, jpath);
+		System.out.println(result);
+		
 	}
 }
