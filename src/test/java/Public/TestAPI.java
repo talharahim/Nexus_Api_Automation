789a73be8b36d3334fb485290ba283e49bcb3021
Talha <talharahim@gmail.com>
2022-07-07 03:19:24 -0700

    y

--- src/test/java/Public/TestAPI.java
+++ src/test/java/Public/TestAPI.java
@@ -19,16 +19,18 @@ public class TestAPI {
 	public static JsonPath jsonPathEvaluator;
 
 	@Test(priority = 6, groups = "SPA")
-	public void getcalculatedocuments_v_2()
-			throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+	public void getcalculatedocuments_v_2() throws InterruptedException {
+//	CommonMethods.CompanyDBRestore();
+		String uri = "/cashiering/receipt/adjust";
+		String ver = "2.4";
+		String payload = "./\\TestData\\recieptAdjust.json";
+		jsonPathEvaluator = CommonMethods.postMethod(payload, uri, ver);
+		Boolean Result = jsonPathEvaluator.get("Receipt.Success");
+		System.out.println(jsonPathEvaluator.get().toString().toString());
+		if (Result == false) {
+			Assert.fail();
+		
 
-		String uri = "/spa/CUSTOMER002/documents/1/1";
-		String ver = "2";
-		String jpath = "./\\TestData\\getcalculateddocumentsv2.json";
-
-		HashMap<String, String> params = new HashMap<String, String>();
-		String result = CommonMethods.getMethod(uri, ver, params, jpath);
-		System.out.println(result);
-
+	}
 	}
 }
