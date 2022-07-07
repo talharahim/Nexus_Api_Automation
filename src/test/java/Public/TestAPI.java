bbbb4b71679a01a9cbe50d300679316ac5d59bef
Talha <talharahim@gmail.com>
2022-07-07 00:30:13 -0700

    up

--- src/test/java/Public/TestAPI.java
+++ src/test/java/Public/TestAPI.java
@@ -19,19 +19,19 @@ public class TestAPI {
 	public static JsonPath jsonPathEvaluator;
 
 	
-	@Test
-	public void TC001_RecieptAdjustment() throws ClassNotFoundException, SQLException, InterruptedException {
-	//	CommonMethods.CompanyDBRestore();
-		String uri = "/cashiering/receipt/adjust";
-		String ver = "2.4";
-		String payload = "./\\TestData\\recieptAdjust.json";
-		jsonPathEvaluator = CommonMethods.postMethod(payload, uri, ver);
-		Boolean Result = jsonPathEvaluator.get("Receipt.Success");
-		System.out.println(jsonPathEvaluator.get().toString().toString());
-		if (Result == false) {
-			Assert.fail();
-		}
+	@Test(priority = 5, groups = "ServiceOrder")
+	public void putaddMeterReading_v_2()
+			throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+
+		String uri = "/serviceOrder/addMeterReading";
+		String ver = "2";
+		String jpath = "./\\TestData\\addMeterReading_v2.json";
+
+		ValidatableResponse result = CommonMethods.putMethod(uri, ver, jpath);
+		result.assertThat().body(Matchers.containsString("true"));
+		result.assertThat().body(Matchers.containsString("READ"));
+		result.assertThat().body(Matchers.containsString("created"));
+		System.out.println(result.extract().asString());
 
 	}
-
 }
