6c0f72651c252f534cad48be1ccf01c8c7f4ff72
Talha <talharahim@gmail.com>
2022-07-13 07:41:19 -0700

    update

--- src/test/java/Public/CashieringPost.java
+++ src/test/java/Public/CashieringPost.java
@@ -16,38 +16,32 @@ public class CashieringPost {
 
 	@Test(priority = 1, groups = "Cashering")
 	public void TC001_saveReciept() throws ClassNotFoundException, SQLException, InterruptedException {
-
-		
 		JsonPath next = CommonMethods.getMethod("/cashiering/receipt/TRREG000001/nextReceipt", "2.4");
-		nextRecieptNumber =next.get("Receipt[0].ReceiptNumber");
-
+		nextRecieptNumber = next.get("Receipt[0].ReceiptNumber");
+		Thread.sleep(5000);
 		String uri = "/cashiering/receipt";
 		String ver = "2.4";
-		
-		
-		//String payload = "./\\TestData\\saveReciept.json";
-		String payload = "{\"Receipt\":{\"ReceiptNumber\":\""+nextRecieptNumber+"\",\"OriginatingReceiptNumber\":\"\",\"Void\":false,\"CustomerId\":\"CUSTOMER008\",\"LocationId\":\"LOCATION007\",\"PaymentOrigin\":\"TEST\",\"CheckbookId\":\"FIRST NATIONAL\",\"PaidBy\":{\"Type\":1,\"Description\":\"\",\"Id\":\"\"},\"Cash\":185.42,\"Check\":{\"Amount\":0,\"Number\":\"\"},\"CreditCard\":{\"Amount\":0},\"Unapplied\":{\"Amount\":0,\"Account\":\"\",\"LocationId\":\"\"},\"Change\":0,\"Comment\":\"ThisisacommenttobesavedintocommentinUMRM102\",\"Document\":[{\"Number\":\"MISC00000000317\",\"LocationId\":\"LOCATION007\",\"StatementNumber\":0,\"ApplyAmount\":185.42,\"OutstandingAmount\":0,\"ReferenceDocumentNumber\":\"\"}]}}";
+		String payload = "{\"Receipt\":{\"ReceiptNumber\":\"" + nextRecieptNumber
+				+ "\",\"OriginatingReceiptNumber\":\"\",\"Void\":false,\"CustomerId\":\"CUSTOMER008\",\"LocationId\":\"LOCATION007\",\"PaymentOrigin\":\"TEST\",\"CheckbookId\":\"FIRST NATIONAL\",\"PaidBy\":{\"Type\":1,\"Description\":\"\",\"Id\":\"\"},\"Cash\":185.42,\"Check\":{\"Amount\":0,\"Number\":\"\"},\"CreditCard\":{\"Amount\":0},\"Unapplied\":{\"Amount\":0,\"Account\":\"\",\"LocationId\":\"\"},\"Change\":0,\"Comment\":\"ThisisacommenttobesavedintocommentinUMRM102\",\"Document\":[{\"Number\":\"MISC00000000317\",\"LocationId\":\"LOCATION007\",\"StatementNumber\":0,\"ApplyAmount\":185.42,\"OutstandingAmount\":0,\"ReferenceDocumentNumber\":\"\"}]}}";
 		jsonPathEvaluator = CommonMethods.postMethodStringPayload(payload, uri, ver);
 		Boolean Result = jsonPathEvaluator.get("Receipt.Success");
-		System.out.println(jsonPathEvaluator.get().toString().toString());
 		if (Result == false) {
 			Assert.fail();
+		} else {
+			System.out.println(jsonPathEvaluator.toString());
 		}
-		
 
 	}
 
 	@Test(priority = 2, groups = "Cashering", dependsOnMethods = "TC001_saveReciept")
 	public void TC002_RecieptAdjustment() throws ClassNotFoundException, SQLException, InterruptedException {
-	
 		String uri = "/cashiering/receipt/adjust";
 		String ver = "2.4";
-		//String payload = "./\\TestData\\recieptAdjust.json";
-		
-		String payload = "{\"Receipt\":{\"ReceiptNumber\":\""+nextRecieptNumber+"\",\"Comment\":\"NexusAPIadjustment\"}}";
+		String payload = "{\"Receipt\":{\"ReceiptNumber\":\"" + nextRecieptNumber
+				+ "\",\"Comment\":\"NexusAPIadjustment\"}}";
 		jsonPathEvaluator = CommonMethods.postMethodStringPayload(payload, uri, ver);
 		Boolean Result = jsonPathEvaluator.get("Receipt.Success");
-		System.out.println(jsonPathEvaluator.get().toString().toString());
+		System.out.println(jsonPathEvaluator.toString());
 		if (Result == false) {
 			Assert.fail();
 		}
