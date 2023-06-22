ecd1c0b4926fc51c188536f8f28988573d5cbd3d
Talha <talharahim@gmail.com>
2023-06-22 07:28:26 -0700

    u

---
+++ src/test/java/Public/checkControllerv4.java
@@ -0,0 +1,34 @@
+package Public;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.NexustAPIAutomation.java.CommonMethods;
+
+import io.restassured.path.json.JsonPath;
+
+public class checkControllerv4 {
+
+	
+	public static JsonPath jsonPathEvaluator;
+
+	@Test(priority = 1, groups = "Cashering")
+	public void getCheckv4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+		// CommonMethods.CompanyDBRestore();
+		String uri = "/check/CHEQ00000000009";
+		String ver = "4.0";
+		String jpath = "./\\TestData\\getCheckv4.json";
+		HashMap<String, String> params = new HashMap<String, String>();
+		//params.put("SearchQuery", "sally");
+
+		String result = CommonMethods.getMethod(uri, ver, params, jpath);
+		System.out.println(result);
+
+
+	}
+
+}
