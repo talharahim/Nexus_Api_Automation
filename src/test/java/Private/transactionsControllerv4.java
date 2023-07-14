a7c727c9bfd58041fe4c90e32b8395979525cbcc
Talha <talharahim@gmail.com>
2023-07-14 05:10:33 -0700

    Update transactionsControllerv4.java

--- src/test/java/Private/transactionsControllerv4.java
+++ src/test/java/Private/transactionsControllerv4.java
@@ -14,19 +14,13 @@ public class transactionsControllerv4 {
 
 	@Test(priority = 1, groups = "Transaction")
 	public void getapplyByService_v4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
-
 		String uri = "/transaction/payment/PYMT00000000405/MISC00000000311/applyByService";
 		String ver = "4.0";
 		String jpath = "./\\TestData\\getapplyByService_v4.json";
-		
-	     HashMap<String, String> params = new HashMap<String, String>();
+		HashMap<String, String> params = new HashMap<String, String>();
 		// params.put("CustomerId", "CUSTOMER012"); 
 		 //params.put("LocationId", "LOCATION011"); 
 		 String result = CommonMethods.getMethod(uri, ver, params, jpath);
 		 System.out.println(result);
 	}
-	
-	
-
-
 }
