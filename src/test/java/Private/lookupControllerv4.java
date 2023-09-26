558c42a96c42156b636b6a074309fe0218b14dae
Talha <talharahim@gmail.com>
2023-09-25 23:59:30 -0700

    .

--- src/test/java/Private/lookupControllerv4.java
+++ src/test/java/Private/lookupControllerv4.java
@@ -102,7 +102,7 @@ public class lookupControllerv4 {
 	public void lookupMeterReadv4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
 		String uri3 = "/lookupMeterRead";
 		String ver = "4.0";
-		String jpath = "./\\TestData\\lookupMeterReadv4.json";
+		String jpath = "./\\TestData\\lookupMeterRead_v4.json";
 	 	HashMap<String, String> params = new HashMap<String, String>();
 		// params.put("Batchsource", "NONE");  //params.put("LocationId", "LOCATION011"); 
 		String result = CommonMethods.getMethod(uri3, ver, params, jpath);
