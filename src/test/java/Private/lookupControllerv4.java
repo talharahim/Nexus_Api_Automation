9596e3e8d40f10fc6a258d0515eae7c369706d28
Talha <talharahim@gmail.com>
2023-10-27 06:57:17 -0700

    quipclass

--- src/test/java/Private/lookupControllerv4.java
+++ src/test/java/Private/lookupControllerv4.java
@@ -140,6 +140,18 @@ public class lookupControllerv4 {
 		String result = CommonMethods.getMethod(uri, ver, params, jpath);
 		System.out.println(result);
 	}
+	
+	
+	@Test(priority = 12, groups = "lookup")
+	public void lookupequipmentClassv4() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+		String uri = "/lookup/EquipmentClass";
+		String ver = "4.0";
+		String jpath = "./\\TestData\\equipmentClass_v4.json";
+	 	HashMap<String, String> params = new HashMap<String, String>();
+	 	params.put("EquipmentClass", "ELECMETER");
+		String result = CommonMethods.getMethod(uri, ver, params, jpath);
+		System.out.println(result);
+	}
 }
 
 	
