f0f2b9bb446b61395756ede030bd4042862d03b7
Talha <talharahim@gmail.com>
2024-03-21 04:03:05 -0700

    .

---
+++ src/test/java/Public/accountBalancesv4.java
@@ -0,0 +1,43 @@
+package Public;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.NexustAPIAutomation.java.CommonMethods;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class accountBalancesv4 {
+
+	public static JsonPath jsonPathEvaluator;
+
+	@Test(priority = 1, groups = "AccountBalances")
+	public void getAccountBalancesV2() throws ClassNotFoundException, SQLException, InterruptedException, IOException {
+
+		String uri = "/accountBalance/getAccountBalances";
+		String ver = "4.0";
+		String jpath = "./TestData\\accountBalancev4.json";
+
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("LocationId", "LOCATION008");
+		params.put("CustomerId", "CUSTOMER009");
+		params.put("UserDate", "2027-04-12");
+		String result = CommonMethods.getMethod(uri, ver, params, jpath);
+		System.out.println(result);
+
+	}
+
+	
+
+}
